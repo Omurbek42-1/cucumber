4278cab251f027276eb852004ba1bec91e48a07c
omurbek <omurbekzaanbaeva@gmail.com>
2024-06-24 22:28:06 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import asyncio 
+import logging
+from aiogram import types  
+
+from config import dp, bot 
+from handlers import(
+    start_router,
+    shop_router, 
+    start_router,
+    picture_router, 
+    echo_router 
+)
+
+
